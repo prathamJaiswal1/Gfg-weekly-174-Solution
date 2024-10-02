f63910640e60909de30491ff32191b735abec39a
Pratham Jaiswal <123011675+prathamJaiswal1@users.noreply.github.com>
2024-10-02 16:04:32 +0530

    Problem4.cpp

--- GFG Weekly Contest - 174/Problem4.cpp
+++ GFG Weekly Contest - 174/Problem4.cpp
@@ -1,114 +1,110 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 struct Node {
-    int maxLen, prefixLen, suffixLen; // longest non-decreasing subarray, prefix and suffix lengths
-    int start, end; // range of values at the ends of the segment
-
-    Node() : maxLen(0), prefixLen(0), suffixLen(0), start(-1), end(-1) {}
-    Node(int value) : maxLen(1), prefixLen(1), suffixLen(1), start(value), end(value) {}
+    int maxlen, prelen, suflen, start, end, len;
+    Node() {
+        maxlen = prelen = suflen = len = 0;
+        start = -1, end = -1;
+    }
+    Node(int val) {
+        maxlen = prelen = suflen = len = 1;
+        start = val, end = val;
+    }
 };
 
-class SegmentTree {
-    vector<Node> tree;
-    vector<int> arr;
+class segmentTree {
+    vector<Node> seg;
     int n;
-
-    Node merge(Node left, Node right) {
-        Node res;
-        res.start = left.start;
-        res.end = right.end;
-
-        // Merge the two segments
-        res.maxLen = max(left.maxLen, right.maxLen);
-        if (left.end <= right.start) { // If the segments can be merged
-            res.maxLen = max(res.maxLen, left.suffixLen + right.prefixLen);
-        }
-        res.prefixLen = left.prefixLen;
-        if (left.prefixLen == (left.end - left.start + 1) && left.end <= right.start) {
-            res.prefixLen += right.prefixLen;
+    
+    Node merge(Node &left, Node &right) {
+        Node par;
+        par.maxlen = max(left.maxlen, right.maxlen);
+        par.start = left.start;
+        par.end = right.end;
+        par.len = left.len + right.len;
+        
+        if (left.end <= right.start) {
+            par.maxlen = max(par.maxlen, left.suflen + right.prelen);
         }
 
-        res.suffixLen = right.suffixLen;
-        if (right.suffixLen == (right.end - right.start + 1) && left.end <= right.start) {
-            res.suffixLen += left.suffixLen;
+        par.prelen = left.prelen;
+        if (left.prelen == left.len && left.end <= right.start) {
+            par.prelen = left.len + right.prelen;
         }
 
-        return res;
+        par.suflen = right.suflen;
+        if (right.suflen == right.len && left.end <= right.start) {
+            par.suflen = left.suflen + right.len;
+        }
+
+        return par;
     }
 
-    void build(int idx, int l, int r) {
-        if (l == r) {
-            tree[idx] = Node(arr[l]);
+    void build(int node, int low, int high, vector<int> &arr) {
+        if (low == high) {
+            seg[node] = Node(arr[low]);
             return;
         }
-        int mid = (l + r) / 2;
-        build(2 * idx, l, mid);
-        build(2 * idx + 1, mid + 1, r);
-        tree[idx] = merge(tree[2 * idx], tree[2 * idx + 1]);
+        int mid = (low + high) >> 1;
+        build(2 * node + 1, low, mid, arr);
+        build(2 * node + 2, mid + 1, high, arr);
+        seg[node] = merge(seg[2 * node + 1], seg[2 * node + 2]);
     }
 
-    Node query(int idx, int l, int r, int ql, int qr) {
-        if (ql > r || qr < l) return Node();
-        if (ql <= l && qr >= r) return tree[idx];
+    void update(int node, int low, int high, int pos, int val) {
+        if (low == high) {
+            seg[node] = Node(val);
+            return;
+        }
+        int mid = (low + high) >> 1;
+        if (pos <= mid) {
+            update(2 * node + 1, low, mid, pos, val);
+        } else {
+            update(2 * node + 2, mid + 1, high, pos, val);
+        }
+        seg[node] = merge(seg[2 * node + 1], seg[2 * node + 2]);
+    }
 
-        int mid = (l + r) / 2;
-        Node left = query(2 * idx, l, mid, ql, qr);
-        Node right = query(2 * idx + 1, mid + 1, r, ql, qr);
+    Node query(int node, int low, int high, int qs, int qe) {
+        if (qe < low || qs > high) {
+            return Node();
+        }
+        if (qs <= low && high <= qe) {
+            return seg[node];
+        }
+        int mid = (low + high) >> 1;
+        Node left = query(2 * node + 1, low, mid, qs, qe);
+        Node right = query(2 * node + 2, mid + 1, high, qs, qe);
         return merge(left, right);
     }
 
-    void update(int idx, int l, int r, int pos, int value) {
-        if (l == r) {
-            // Update the value at position pos
-            arr[l] = value;
-            tree[idx] = Node(value);
-            return;
-        }
-
-        int mid = (l + r) / 2;
-        if (pos <= mid) {
-            update(2 * idx, l, mid, pos, value); // Update the left child
-        } else {
-            update(2 * idx + 1, mid + 1, r, pos, value); // Update the right child
-        }
-        // Rebuild the current node after the update
-        tree[idx] = merge(tree[2 * idx], tree[2 * idx + 1]);
-    }
-
 public:
-    SegmentTree(const vector<int>& inputArr) {
-        arr = inputArr;
+    segmentTree(vector<int> &arr) {
         n = arr.size();
-        tree.resize(4 * n);
-        build(1, 0, n - 1);
+        seg.resize(4 * n);
+        build(0, 0, n - 1, arr);
     }
 
-    int query(int l, int r) {
-        Node res = query(1, 0, n - 1, l, r);
-        return res.maxLen;
+    void update(int pos, int val) {
+        update(0, 0, n - 1, pos, val);
     }
 
-    void update(int pos, int value) {
-        update(1, 0, n - 1, pos, value);
+    Node query(int qs, int qe) {
+        return query(0, 0, n - 1, qs, qe);
     }
 };
-void solve(){
-    int n,q;cin>>n>>q;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++)cin>>arr[i];
-    SegmentTree st(arr);
-    while(q--){
-        int type,qs,qe;cin>>type>>qs>>qe;
-        if(type==0){
-            cout<<st.query(qs,qe)<<endl;
-        }else{
-            st.update(qs,qe);
-        }
-    }
 
-}
-int main(){
-    solve();
-    return 0;
-}
+class Solution {
+public:
+    vector<int> longestSubarray(int n, vector<int> &arr, int m, vector<vector<int>> &q) {
+        segmentTree st(arr);
+        vector<int> ans;
+        for (int i = 0; i < m; i++) {
+            int a = q[i][0], b = q[i][1], c = q[i][2];
+            if (a == 0) {
+                ans.push_back(st.query(b, c).maxlen);
+            } else {
+                st.update(b, c); 
+            }
+        }
+        return ans;
+    }
+};
